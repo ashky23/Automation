17204aeb6ad0deffd50932bac9585344ff532f18
freakash <iamakashkumar1999@gmail.com>
2018-08-19 16:09:10 +0530

    Selenium Package

---
+++ selenium/twitter.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+user="8351804083"
+pwd="akky$$44"
+browser=webdriver.Firefox(executable_path=r'/home/freakash/python/selenium/geckodriver')
+browser.get('https://www.twitter.com')
+assert "Twitter" in browser.title
+elem=browser.find_element_by_name("session[username_or_email]")
+elem.send_keys(user)
+elem=browser.find_element_by_name("session[password]")
+elem.send_keys(pwd)
+browser.find_element_by_xpath('/html/body/div[1]/div/div[1]/div[1]/div[1]/form/input[1]').click()
+#elem.send_keys(Keys.RETURN)
+#browser.close()
