5d65389f727f135441fe88bf7a03e84395357990
AKASH KUMAR <33580834+freakash@users.noreply.github.com>
2019-01-01 16:08:41 +0530

    Read one joke at every run of this file

--- Request_json.py
+++ Request_json.py
@@ -3,7 +3,7 @@ url="https://icanhazdadjoke.com/"
 response=requests.get(url,headers={"Accept":"application/json"})
 #------------------------------------------------+
 #text/html---we'll get html files                |
-#text/plain---we'll get text files		 |
+#text/plain---we'll get text files		           |
 #application/json---we'll get json file format   |
 #------------------------------------------------+
 print(response.text)
